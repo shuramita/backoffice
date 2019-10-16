c8a4ba9836f198b5750f9fdd03983b99a630e319
Tam <minhtamtt@gmail.com>
2019-10-16 11:05:40 +0700

    init source

--- webpack.js
+++ webpack.js
@@ -1,12 +1,8 @@
-exports.assets = {
-    // sass:'./packages/shura/backoffice/src/assets/sass/',
-    // js:'./packages/shura/backoffice/src/assets/js/',
-    "assets":{
-        "sass":[
-            ["backoffice.scss","backoffice.css"]
-        ],
-        "js":[
-            ["backoffice.js","backoffice.js"]
-        ]
-    }
+exports.combine = function(mix){
+    let source = `${__dirname}/src`;
+    let dest = 'public';
+    mix.sass(`${source}/assets/sass/backoffice.scss`, `${dest}/css/backoffice/backoffice.css`).version();
+    mix.js([
+        `${source}/assets/js/backoffice.js`
+    ], `${dest}/js/backoffice/backoffice.js`).version();
 };
