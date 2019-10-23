93df27366f6f38561f6c820b07df3d58ed6f84d7
Tam <minhtamtt@gmail.com>
2019-10-23 22:20:17 +0700

    update document

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -7,19 +7,4 @@ use App\Http\Controllers\Controller as AppController;
 class Controller extends AppController
 {
     public $namespace = 'BackOffice'; // registered in Service Provider
-
-    function validationError($message){
-        $validate = (object)[
-            'error'=>(object) ['status'=>'ERROR','message'=>$message],
-            'data'=>$message
-        ];
-        return response()->json($validate);
-    }
-    function jsonResponse($data,$message=""){
-        $response = (object)[
-            'error'=>(object) ['status'=>'OK','message'=>$message],
-            'data'=>$data
-        ];
-        return response()->json($response);
-    }
 }
